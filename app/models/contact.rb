5d126c6276cb316078774cb6094af969f21427c9
Lincxx <bossco28@gmail.com>
2021-12-30 09:18:28 -0500

    Add note model, migration, contact model added class and instance methods

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,22 @@
 class Contact < ApplicationRecord
     # validates :name, presence: true
+    has_many :notes
+
+    # class method start with self
+    def self.by_age_and_limit(num)
+        Contact.order(:age).limit(num)
+    end
+
+    # instance method, this is a called on the instance of the class
+    # self refers to the instance of the class calling the method
+    def about
+        "Contact name: #{self.name}, age is #{self.age}"
+        get_password
+    end
+
+
+    private
+    def get_password
+        123456
+    end
 end
